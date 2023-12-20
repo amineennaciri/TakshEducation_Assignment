22f3f86daebc847803198fe5d880134bc8a73345
amineennaciri <spitlight.enn@gmail.com>
2023-12-20 20:21:25 +0100

    update

---
+++ client/src/homeMcqs/DisplayMcqs.jsx
@@ -0,0 +1,59 @@
+function DisplayMcqs({mcq}) {
+  return (
+    <div className = 'flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
+                      <div className='my-4'>
+                          <span className='text-xl mr-4 text-gray-500'>
+                              Id
+                          </span>
+                          <span>
+                              {mcq._id}
+                          </span>
+                      </div>
+                      <div className='my-4'>
+                          <span className='text-xl mr-4 text-gray-500'>
+                              Title
+                          </span>
+                          <span>
+                              {mcq.question}
+                          </span>
+                      </div>
+                      <div className='my-4'>
+                          <span className='text-xl mr-4 text-gray-500'>
+                              Options
+                          </span>
+                          {mcq.options}
+                          {/*<ol className="list-decimal">
+                             {mcqs.options.map((el,index)=>(
+                                <li key={index}>{el}</li>
+                              ))}
+                            </ol> */}
+                      </div>
+                      <div className='my-4'>
+                          <span className='text-xl mr-4 text-gray-500'>
+                              MCQ's Answer Index
+                          </span>
+                          <span>
+                          {mcq.correctOptionIndex+1}
+                          </span>
+                      </div>
+                      <div className='my-4'>
+                          <span className='text-xl mr-4 text-gray-500'>
+                              Exam's Id
+                          </span>
+                          <span>
+                          {mcq.author}
+                          </span>
+                      </div>
+                      <div className='my-4'>
+                          <span className='text-xl mr-4 text-gray-500'>
+                              Time of creation
+                          </span>
+                          <span>
+                              {new Date(mcq.timestamp).toString()}
+                          </span>
+                      </div>
+                  </div>
+  )
+}
+
+export default DisplayMcqs
--- client/src/pages/HomeMcqs.jsx
+++ client/src/pages/HomeMcqs.jsx
@@ -3,10 +3,10 @@ import axios from 'axios';
 import {useParams} from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
-import OrderedList from '../homeMcqs/OrderedList';
+import DisplayMcqs from '../homeMcqs/DisplayMcqs';
 
 function HomeMcqs() {
-    const [mcqs, setMcqs] = useState({});
+    const [mcqsData, setMcqsData] = useState({});
   const [loading, setLoading] = useState(false);
   const { examId } = useParams();
 
@@ -15,7 +15,7 @@ function HomeMcqs() {
     axios.get(`http://localhost:8000/exams/${examId}/mcqs`)
     .then((response) =>{
       console.log(response.data.data);
-      setMcqs(response.data.data[0]);
+      setMcqsData(response.data.data);
         setLoading(false);
     })
     .catch((error)=>{
@@ -30,61 +30,10 @@ function HomeMcqs() {
           {
               loading ? (
                   <Spinner/>
-              ) : (
-                  <div className = 'flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
-                      <div className='my-4'>
-                          <span className='text-xl mr-4 text-gray-500'>
-                              Id
-                          </span>
-                          <span>
-                              {mcqs._id}
-                          </span>
-                      </div>
-                      <div className='my-4'>
-                          <span className='text-xl mr-4 text-gray-500'>
-                              Title
-                          </span>
-                          <span>
-                              {mcqs.question}
-                          </span>
-                      </div>
-                      <div className='my-4'>
-                          <span className='text-xl mr-4 text-gray-500'>
-                              Options
-                          </span>
-                          {mcqs.options}
-                          {/*<ol className="list-decimal">
-                             {mcqs.options.map((el,index)=>(
-                                <li key={index}>{el}</li>
-                              ))}
-                            </ol> */}
-                      </div>
-                      <div className='my-4'>
-                          <span className='text-xl mr-4 text-gray-500'>
-                              MCQ's Answer Index
-                          </span>
-                          <span>
-                          {mcqs.correctOptionIndex+1}
-                          </span>
-                      </div>
-                      <div className='my-4'>
-                          <span className='text-xl mr-4 text-gray-500'>
-                              Exam's Id
-                          </span>
-                          <span>
-                          {mcqs.author}
-                          </span>
-                      </div>
-                      <div className='my-4'>
-                          <span className='text-xl mr-4 text-gray-500'>
-                              Time of creation
-                          </span>
-                          <span>
-                              {new Date(mcqs.timestamp).toString()}
-                          </span>
-                      </div>
-                  </div>
-              )
+              ) : 
+              mcqsData.map((el)=>(
+                    <DisplayMcqs key={el._id} mcq={el}/>
+                ))
           }
       </div>
   )
