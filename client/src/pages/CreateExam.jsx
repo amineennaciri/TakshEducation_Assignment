ef7c0b447453e2c7e3c792252c4404d8736ef591
amineennaciri <spitlight.enn@gmail.com>
2023-12-20 18:24:48 +0100

    client side can now Show in detail a book but also edit it and comminute with the rest api in backend

--- client/src/pages/CreateExam.jsx
+++ client/src/pages/CreateExam.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import {useState} from 'react';
+import axios from 'axios';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import { useNavigate } from 'react-router-dom';
 
 function CreateExam() {
-  return (
-    <div></div>
-  )
+    const [title, setTitle] = useState('');
+    const [duration, setDuration] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const handleSaveExam = () => {
+        const data = {
+            title,
+            duration,
+        };
+        setLoading(true);
+        axios.post('http://localhost:8000/addexam', data)
+        .then(()=>{
+            setLoading(false);
+            navigate('/');
+        })
+        .catch((error)=>{
+            setLoading(false);
+            alert('An error happened. Please check console');
+            console.log(error);
+        })
+    };
+
+    return (
+        <div className='p-4'>
+            <BackButton />
+            <h1 className='text-3xl my-4'>Create Exam</h1>
+            {loading ? <Spinner/> 
+            : ''}
+            <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+                <div className='my-4'>
+                    <label className='text-xl mr-4 text-gray-500'>Title</label>
+                    <input type="text" value={title} onChange={(e)=> setTitle(e.target.value)}
+                    className="border-2 border-gray-500 px-4 py-2 w-full" />
+                </div>
+                <div className='my-4'>
+                    <label className="text-xl mr-4 text-gray-500">Duration</label>
+                    <input type="number" value={duration} onChange={(e)=>setDuration(e.target.value)} className='border-2 border-gray-500 px-4 py-2 w-full' />
+                </div>
+                <button className='p-2 bg-sky-300 m-8' onClick={handleSaveExam}>
+                    Save
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default CreateExam
--- client/src/pages/ShowExam.jsx
+++ client/src/pages/ShowExam.jsx
@@ -1,8 +1,74 @@
-import React from 'react'
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import {useParams} from 'react-router-dom';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
 
 function ShowExam() {
+  const [exam, setExam] = useState({});
+  const [loading, setLoading] = useState(false);
+  const { examId } = useParams();
+  console.log(examId);
+
+  useEffect(()=>{
+    setLoading(true);
+    axios.get(`http://localhost:8000/exams/${examId}`)
+    .then((response) =>{
+      console.log(response.data);
+      setExam(response.data);
+        setLoading(false);
+    })
+    .catch((error)=>{
+        console.log(error);
+        setLoading(false);
+    })
+  }, [])
+
   return (
-    <div>ShowExam</div>
+      <div className='p-4'>
+          <BackButton />
+          <h1 className='text-3xl my-4'>Show Exam</h1>
+          {
+              loading ? (
+                  <Spinner/>
+              ) : (
+                  <div className = 'flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
+                      <div className='my-4'>
+                          <span className='text-xl mr-4 text-gray-500'>
+                              Id
+                          </span>
+                          <span>
+                              {exam._id}
+                          </span>
+                      </div>
+                      <div className='my-4'>
+                          <span className='text-xl mr-4 text-gray-500'>
+                              Title
+                          </span>
+                          <span>
+                              {exam.title}
+                          </span>
+                      </div>
+                      <div className='my-4'>
+                          <span className='text-xl mr-4 text-gray-500'>
+                              Duration
+                          </span>
+                          <span>
+                              {exam.duration}
+                          </span>
+                      </div>
+                      <div className='my-4'>
+                          <span className='text-xl mr-4 text-gray-500'>
+                              Time of creation
+                          </span>
+                          <span>
+                              {new Date(exam.timestamp).toString()}
+                          </span>
+                      </div>
+                  </div>
+              )
+          }
+      </div>
   )
 }
 
